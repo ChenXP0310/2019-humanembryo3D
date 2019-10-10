e5d0dc26b39e264dd341666a858b7ea13f6c430c
ChenXP0310 <56373623+ChenXP0310@users.noreply.github.com>
2019-10-10 14:01:56 +0800

    Add files via upload

---
+++ ABcompartments_homer.sh
@@ -0,0 +1,19 @@
+for sm in  blastocyst
+do
+species=human
+genome=hg19
+res=100k
+superes=400k
+resk=100000
+superesk=400000
+RE=GATC
+
+makeTagDirectory ${sm}_un -format HiCsummary homer.$sm.summary.txt
+cp -pr ${sm}_un ${sm}_pro
+makeTagDirectory ${sm}_pro -update -genome /home/chenxp/anaconda2/homer/data/genomes/$genome/ -removePEbg -restrictionSite $RE -both -removeSelfLigation
+
+runHiCpca.pl $sm.$res.$superesCompartment.Homer ${sm}_pro -res $resk -superRes $superesk -genome $genome -pc 3 
+
+rm -r ${sm}_un
+done
+
